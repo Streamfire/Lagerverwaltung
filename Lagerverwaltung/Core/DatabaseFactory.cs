5bfcd6a5bad0266e209b667300d3ca8e03c2308c
CKextreme <CKextreme@users.noreply.github.com>
2018-10-10 16:50:03 +0200

    Struktur MVC-System
    Kleine Strukturänderungen und Test-Interfaces implementiert.
    Kann sich demnächst noch ändern.
    Eventuell Abstrakte Klassen falls besser.

---
+++ Lagerverwaltung/Core/DatabaseFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lagerverwaltung.Core
+{
+    class DatabaseFactory
+    {
+        //private static _factory;
+        //private static Databse _connection;
+
+            /*
+        public static object GetFactory()
+        {
+            if()
+            {
+
+            }
+            return
+        }
+        */
+
+        /*
+         public static object GetConnection()
+         {
+
+        }
+         */
+    }
+}
---
+++ Lagerverwaltung/Core/Interfaces/IBasic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lagerverwaltung.Core.Interfaces
+{
+    interface IBasic //: IEquatable<IBasic>, IEnumerable<IBasic>, IEnumerator<IBasic>, IComparable<IBasic>
+    {
+        float Height { get; set; }
+        float Width { get; set; }
+        float Length { get; set; }
+    }
+}
