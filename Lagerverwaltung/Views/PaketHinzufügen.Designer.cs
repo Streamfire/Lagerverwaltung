b934c33e53fb0e61f452bdb75a1e581ff4bffcb6
Stupidjolls <43861209+Stupidjolls@users.noreply.github.com>
2018-11-08 18:47:25 +0100

    GUI Paket hinzufügen

---
+++ Lagerverwaltung/Views/PaketHinzufügen.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Lagerverwaltung.Views
+{
+    partial class PaketHinzufügen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxPaketbezeichnung = new System.Windows.Forms.TextBox();
+            this.textBoxAnschaffungsgrund = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonPaketHinzufügen = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxHoehe = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxLaenge = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBoxBreite = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.buttonProduktAuswählen = new System.Windows.Forms.Button();
+            this.labelProduktAusgewählt = new System.Windows.Forms.Label();
+            this.buttonAbbrechen = new System.Windows.Forms.Button();
+            this.dateTimePickerHaltbarkeit = new System.Windows.Forms.DateTimePicker();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Paketbezeichnung";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Anschaffungsgrund";
+            // 
+            // textBoxPaketbezeichnung
+            // 
+            this.textBoxPaketbezeichnung.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPaketbezeichnung.Location = new System.Drawing.Point(246, 105);
+            this.textBoxPaketbezeichnung.Name = "textBoxPaketbezeichnung";
+            this.textBoxPaketbezeichnung.Size = new System.Drawing.Size(274, 26);
+            this.textBoxPaketbezeichnung.TabIndex = 2;
+            // 
+            // textBoxAnschaffungsgrund
+            // 
+            this.textBoxAnschaffungsgrund.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAnschaffungsgrund.Location = new System.Drawing.Point(246, 162);
+            this.textBoxAnschaffungsgrund.Name = "textBoxAnschaffungsgrund";
+            this.textBoxAnschaffungsgrund.Size = new System.Drawing.Size(274, 26);
+            this.textBoxAnschaffungsgrund.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(31, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Haltbarkeit";
+            // 
+            // buttonPaketHinzufügen
+            // 
+            this.buttonPaketHinzufügen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPaketHinzufügen.Location = new System.Drawing.Point(419, 480);
+            this.buttonPaketHinzufügen.Name = "buttonPaketHinzufügen";
+            this.buttonPaketHinzufügen.Size = new System.Drawing.Size(143, 45);
+            this.buttonPaketHinzufügen.TabIndex = 6;
+            this.buttonPaketHinzufügen.Text = "Paket hinzufügen";
+            this.buttonPaketHinzufügen.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(31, 280);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Größe:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(121, 281);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Höhe";
+            // 
+            // textBoxHoehe
+            // 
+            this.textBoxHoehe.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxHoehe.Location = new System.Drawing.Point(246, 277);
+            this.textBoxHoehe.Name = "textBoxHoehe";
+            this.textBoxHoehe.Size = new System.Drawing.Size(47, 26);
+            this.textBoxHoehe.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(299, 281);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "mm";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(121, 327);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 20);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Länge";
+            // 
+            // textBoxLaenge
+            // 
+            this.textBoxLaenge.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxLaenge.Location = new System.Drawing.Point(246, 323);
+            this.textBoxLaenge.Name = "textBoxLaenge";
+            this.textBoxLaenge.Size = new System.Drawing.Size(47, 26);
+            this.textBoxLaenge.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(299, 327);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "mm";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(121, 373);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(51, 20);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Breite";
+            // 
+            // textBoxBreite
+            // 
+            this.textBoxBreite.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxBreite.Location = new System.Drawing.Point(246, 369);
+            this.textBoxBreite.Name = "textBoxBreite";
+            this.textBoxBreite.Size = new System.Drawing.Size(47, 26);
+            this.textBoxBreite.TabIndex = 17;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(299, 373);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(35, 20);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "mm";
+            // 
+            // buttonProduktAuswählen
+            // 
+            this.buttonProduktAuswählen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonProduktAuswählen.Location = new System.Drawing.Point(544, 353);
+            this.buttonProduktAuswählen.Name = "buttonProduktAuswählen";
+            this.buttonProduktAuswählen.Size = new System.Drawing.Size(189, 40);
+            this.buttonProduktAuswählen.TabIndex = 19;
+            this.buttonProduktAuswählen.Text = "Produkt auswählen";
+            this.buttonProduktAuswählen.UseVisualStyleBackColor = true;
+            // 
+            // labelProduktAusgewählt
+            // 
+            this.labelProduktAusgewählt.AutoSize = true;
+            this.labelProduktAusgewählt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelProduktAusgewählt.Location = new System.Drawing.Point(572, 313);
+            this.labelProduktAusgewählt.Name = "labelProduktAusgewählt";
+            this.labelProduktAusgewählt.Size = new System.Drawing.Size(128, 20);
+            this.labelProduktAusgewählt.TabIndex = 20;
+            this.labelProduktAusgewählt.Text = "Produkt Auswahl";
+            // 
+            // buttonAbbrechen
+            // 
+            this.buttonAbbrechen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAbbrechen.Location = new System.Drawing.Point(590, 480);
+            this.buttonAbbrechen.Name = "buttonAbbrechen";
+            this.buttonAbbrechen.Size = new System.Drawing.Size(143, 45);
+            this.buttonAbbrechen.TabIndex = 21;
+            this.buttonAbbrechen.Text = "Abbrechen";
+            this.buttonAbbrechen.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePickerHaltbarkeit
+            // 
+            this.dateTimePickerHaltbarkeit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerHaltbarkeit.Location = new System.Drawing.Point(246, 217);
+            this.dateTimePickerHaltbarkeit.Name = "dateTimePickerHaltbarkeit";
+            this.dateTimePickerHaltbarkeit.Size = new System.Drawing.Size(274, 26);
+            this.dateTimePickerHaltbarkeit.TabIndex = 22;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(28, 18);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(284, 39);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "Paket hinzufügen";
+            // 
+            // PaketHinzufügen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.dateTimePickerHaltbarkeit);
+            this.Controls.Add(this.buttonAbbrechen);
+            this.Controls.Add(this.labelProduktAusgewählt);
+            this.Controls.Add(this.buttonProduktAuswählen);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBoxBreite);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxLaenge);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxHoehe);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonPaketHinzufügen);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxAnschaffungsgrund);
+            this.Controls.Add(this.textBoxPaketbezeichnung);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "PaketHinzufügen";
+            this.Text = "PaketHinzufügen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxPaketbezeichnung;
+        private System.Windows.Forms.TextBox textBoxAnschaffungsgrund;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonPaketHinzufügen;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxHoehe;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxLaenge;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBoxBreite;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button buttonProduktAuswählen;
+        private System.Windows.Forms.Label labelProduktAusgewählt;
+        private System.Windows.Forms.Button buttonAbbrechen;
+        private System.Windows.Forms.DateTimePicker dateTimePickerHaltbarkeit;
+        private System.Windows.Forms.Label label9;
+    }
+}
