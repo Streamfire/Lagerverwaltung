c460642a28fd68f4fec4cc9315834ba134f29862
Henker666 <43860518+Henker666@users.noreply.github.com>
2018-11-08 16:54:17 +0100

    Mini Änderungen an der GUI Lagerverwaltung
    Name in Paket hinzufügen und Paket entfernen geändert und erstee sinnvollte Namen für die Buttons gegeben

--- Lagerverwaltung/Views/Verwaltung.Designer.cs
+++ Lagerverwaltung/Views/Verwaltung.Designer.cs
@@ -71,132 +71,132 @@ namespace Lagerverwaltung.Views
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.tabControl1 = new System.Windows.Forms.TabControl();
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
-			this.button3 = new System.Windows.Forms.Button();
-			this.button4 = new System.Windows.Forms.Button();
-			this.button5 = new System.Windows.Forms.Button();
-			this.button6 = new System.Windows.Forms.Button();
-			this.button7 = new System.Windows.Forms.Button();
-			this.button8 = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// tabControl1
-			// 
-			this.tabControl1.Location = new System.Drawing.Point(-2, 53);
-			this.tabControl1.Name = "tabControl1";
-			this.tabControl1.SelectedIndex = 0;
-			this.tabControl1.Size = new System.Drawing.Size(1127, 548);
-			this.tabControl1.TabIndex = 0;
-			// 
-			// button1
-			// 
-			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button1.Location = new System.Drawing.Point(2, 1);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(110, 46);
-			this.button1.TabIndex = 1;
-			this.button1.Text = "Wareneingang";
-			this.button1.UseVisualStyleBackColor = true;
-			// 
-			// button2
-			// 
-			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button2.Location = new System.Drawing.Point(118, 1);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(113, 46);
-			this.button2.TabIndex = 2;
-			this.button2.Text = "Warenausgang";
-			this.button2.UseVisualStyleBackColor = true;
-			// 
-			// button3
-			// 
-			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button3.Location = new System.Drawing.Point(1006, 1);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(113, 46);
-			this.button3.TabIndex = 3;
-			this.button3.Text = "Lager Entfernen";
-			this.button3.UseVisualStyleBackColor = true;
-			// 
-			// button4
-			// 
-			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button4.Location = new System.Drawing.Point(887, 1);
-			this.button4.Name = "button4";
-			this.button4.Size = new System.Drawing.Size(113, 46);
-			this.button4.TabIndex = 4;
-			this.button4.Text = "Lager Hinzufügen";
-			this.button4.UseVisualStyleBackColor = true;
-			// 
-			// button5
-			// 
-			this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button5.Location = new System.Drawing.Point(768, 1);
-			this.button5.Name = "button5";
-			this.button5.Size = new System.Drawing.Size(113, 46);
-			this.button5.TabIndex = 5;
-			this.button5.Text = "Lagerung Optimieren";
-			this.button5.UseVisualStyleBackColor = true;
-			// 
-			// button6
-			// 
-			this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button6.Location = new System.Drawing.Point(237, 1);
-			this.button6.Name = "button6";
-			this.button6.Size = new System.Drawing.Size(113, 46);
-			this.button6.TabIndex = 6;
-			this.button6.Text = "Ware umlagern";
-			this.button6.UseVisualStyleBackColor = true;
-			// 
-			// button7
-			// 
-			this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button7.Location = new System.Drawing.Point(426, 1);
-			this.button7.Name = "button7";
-			this.button7.Size = new System.Drawing.Size(113, 46);
-			this.button7.TabIndex = 8;
-			this.button7.Text = "Regal Hinzufügen";
-			this.button7.UseVisualStyleBackColor = true;
-			// 
-			// button8
-			// 
-			this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button8.Location = new System.Drawing.Point(545, 1);
-			this.button8.Name = "button8";
-			this.button8.Size = new System.Drawing.Size(113, 46);
-			this.button8.TabIndex = 7;
-			this.button8.Text = "Lager Entfernen";
-			this.button8.UseVisualStyleBackColor = true;
-			// 
-			// Verwaltung
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1127, 602);
-			this.Controls.Add(this.button7);
-			this.Controls.Add(this.button8);
-			this.Controls.Add(this.button6);
-			this.Controls.Add(this.button5);
-			this.Controls.Add(this.button4);
-			this.Controls.Add(this.button3);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.tabControl1);
-			this.Name = "Verwaltung";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "Verwaltung";
-			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.VerwaltungFormClosing);
-			this.ResumeLayout(false);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.buttonPaketHinzufuegen = new System.Windows.Forms.Button();
+            this.buttonPaketEntfernen = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Location = new System.Drawing.Point(-2, 53);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1127, 548);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // buttonPaketHinzufuegen
+            // 
+            this.buttonPaketHinzufuegen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPaketHinzufuegen.Location = new System.Drawing.Point(2, 1);
+            this.buttonPaketHinzufuegen.Name = "buttonPaketHinzufuegen";
+            this.buttonPaketHinzufuegen.Size = new System.Drawing.Size(110, 46);
+            this.buttonPaketHinzufuegen.TabIndex = 1;
+            this.buttonPaketHinzufuegen.Text = "Paket Hinzufügen";
+            this.buttonPaketHinzufuegen.UseVisualStyleBackColor = true;
+            // 
+            // buttonPaketEntfernen
+            // 
+            this.buttonPaketEntfernen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPaketEntfernen.Location = new System.Drawing.Point(118, 1);
+            this.buttonPaketEntfernen.Name = "buttonPaketEntfernen";
+            this.buttonPaketEntfernen.Size = new System.Drawing.Size(113, 46);
+            this.buttonPaketEntfernen.TabIndex = 2;
+            this.buttonPaketEntfernen.Text = "Paket entfernen";
+            this.buttonPaketEntfernen.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(1006, 1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 46);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Lager Entfernen";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(887, 1);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 46);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Lager Hinzufügen";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(768, 1);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(113, 46);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Lagerung Optimieren";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(237, 1);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(113, 46);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Ware umlagern";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(426, 1);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(113, 46);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Regal Hinzufügen";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(545, 1);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(113, 46);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Lager Entfernen";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // Verwaltung
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1127, 602);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.buttonPaketEntfernen);
+            this.Controls.Add(this.buttonPaketHinzufuegen);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Verwaltung";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Verwaltung";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.VerwaltungFormClosing);
+            this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.TabControl tabControl1;
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button buttonPaketHinzufuegen;
+		private System.Windows.Forms.Button buttonPaketEntfernen;
 		private System.Windows.Forms.Button button3;
 		private System.Windows.Forms.Button button4;
 		private System.Windows.Forms.Button button5;
