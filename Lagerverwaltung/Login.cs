699a6c27f630d720f6cf835d89ac9714e17a7246
z0mbiecat <44175549+z0mbiecat@users.noreply.github.com>
2018-12-18 03:40:21 +0100

    Added: Cleartext Button, Password Change fully implemented, Interface add user

---
+++ Lagerverwaltung/Login.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lagerverwaltung.Views
+{
+   
+    public partial class Login : Form
+	{
+        public Login()
+		{
+			InitializeComponent();
+		}
+
+		//Login Button gedrückt
+		private void Button1_Click(object sender, EventArgs e)
+		{
+            if (Controller.AuthenticationController.Login(textBox1.Text, textBox2.Text)) 
+			{
+                Controller.AuthenticationController.Adminuser = true;
+                DialogResult = DialogResult.OK;
+                Close();
+			}
+			else
+			{
+				label4.Text = "Nutzername oder Passwort falsch.";
+                label4.Visible = true;
+                Controller.AuthenticationController.Adminuser = false;
+            }
+		}
+
+		private void Button2_Click(object sender, EventArgs e)
+		{
+            Controller.AuthenticationController.Adminuser = false;
+            DialogResult = DialogResult.OK;
+			Close();
+		}
+
+        private void ButtonPasswortAendern_Click(object sender, EventArgs e)
+        {
+            using (var passwortaendern = new PasswortAendern())
+            {
+                passwortaendern.ShowDialog();
+            }
+        }
+
+        private void show_cleartext_Click(object sender, EventArgs e)
+        {
+            textBox2.UseSystemPasswordChar = !textBox2.UseSystemPasswordChar;
+        }
+    }
+}
---
+++ Lagerverwaltung/PasswortAendern.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lagerverwaltung.Views
+{
+    public partial class PasswortAendern : Form
+    {
+        public PasswortAendern()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void ButtonAbbrechen_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ButtonOkay_Click(object sender, EventArgs e)
+        {
+            if(textBoxNeuesPasswort.Text != textBoxNeuesPasswortBestaetigen.Text)
+            {
+                MessageBox.Show("Passwörter stimmen nicht überein!", "Passwortänderung fehlgeschlagen!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (Lagerverwaltung.Controller.AuthenticationController.Login(textBoxNutzername.Text, textBoxAltesPasswort.Text))         
+            {
+                try
+                {
+                    //Erstellung Salt + Passwordhash und update in Database
+                    string temp_salt = Lagerverwaltung.Controller.AuthenticationController.Create_Salt(10);
+                    DB.UserSQL.UpdateUserPassword(textBoxNutzername.Text, Lagerverwaltung.Controller.AuthenticationController.GeneratePasswordHash(textBoxNeuesPasswort.Text, temp_salt), temp_salt);
+                }
+                catch
+                {
+                    MessageBox.Show("Bitte vorher die PostgreSQL Datenbank starten!", "Keine Verbindung!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Application.Exit();
+                }
+                Close();
+            }
+            else
+                MessageBox.Show("Der Nutzername oder das Passwort sind ungültig!", "Passwortänderung fehlgeschlagen!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void show_cleartext_Click_1(object sender, EventArgs e)
+        {
+            textBoxAltesPasswort.UseSystemPasswordChar = !textBoxAltesPasswort.UseSystemPasswordChar;
+            textBoxNeuesPasswort.UseSystemPasswordChar = !textBoxNeuesPasswort.UseSystemPasswordChar;
+            textBoxNeuesPasswortBestaetigen.UseSystemPasswordChar = !textBoxNeuesPasswortBestaetigen.UseSystemPasswordChar; 
+        }
+    }
+}
