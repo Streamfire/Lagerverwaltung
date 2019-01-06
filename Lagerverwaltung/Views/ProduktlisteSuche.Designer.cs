f1f99a874a357b2955fb9415d4d4d79f93d00812
5000totalBTW <43860627+5000totalBTW@users.noreply.github.com>
2019-01-06 12:45:42 +0100

    Comitten der neuen Dateien

---
+++ Lagerverwaltung/Views/ProduktlisteSuche.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Lagerverwaltung.Views
+{
+    partial class ProduktlisteSuche
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Schliessen = new System.Windows.Forms.Button();
+            this.Auswaehlen = new System.Windows.Forms.Button();
+            this.Produkt_hinzufuegen = new System.Windows.Forms.Button();
+            this.Produkt_aendern = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.produkt_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gewicht = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.preis = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.zeichnungsnummer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Schliessen
+            // 
+            this.Schliessen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Schliessen.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Schliessen.Location = new System.Drawing.Point(599, 685);
+            this.Schliessen.Name = "Schliessen";
+            this.Schliessen.Size = new System.Drawing.Size(120, 35);
+            this.Schliessen.TabIndex = 1;
+            this.Schliessen.Text = "Abbrechen";
+            this.Schliessen.UseVisualStyleBackColor = true;
+            this.Schliessen.Click += new System.EventHandler(this.Schliessen_Click);
+            // 
+            // Auswaehlen
+            // 
+            this.Auswaehlen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Auswaehlen.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Auswaehlen.Location = new System.Drawing.Point(449, 685);
+            this.Auswaehlen.Name = "Auswaehlen";
+            this.Auswaehlen.Size = new System.Drawing.Size(120, 35);
+            this.Auswaehlen.TabIndex = 2;
+            this.Auswaehlen.Text = "Auswählen";
+            this.Auswaehlen.UseVisualStyleBackColor = true;
+            this.Auswaehlen.Click += new System.EventHandler(this.Auswaehlen_Click);
+            // 
+            // Produkt_hinzufuegen
+            // 
+            this.Produkt_hinzufuegen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Produkt_hinzufuegen.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Produkt_hinzufuegen.Location = new System.Drawing.Point(29, 685);
+            this.Produkt_hinzufuegen.Name = "Produkt_hinzufuegen";
+            this.Produkt_hinzufuegen.Size = new System.Drawing.Size(157, 35);
+            this.Produkt_hinzufuegen.TabIndex = 3;
+            this.Produkt_hinzufuegen.Text = "Produkt hinzufügen";
+            this.Produkt_hinzufuegen.UseVisualStyleBackColor = true;
+            this.Produkt_hinzufuegen.Click += new System.EventHandler(this.Produkt_hinzufuegen_Click);
+            // 
+            // Produkt_aendern
+            // 
+            this.Produkt_aendern.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Produkt_aendern.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Produkt_aendern.Location = new System.Drawing.Point(209, 685);
+            this.Produkt_aendern.Name = "Produkt_aendern";
+            this.Produkt_aendern.Size = new System.Drawing.Size(175, 35);
+            this.Produkt_aendern.TabIndex = 4;
+            this.Produkt_aendern.Text = "Produkt ändern";
+            this.Produkt_aendern.UseVisualStyleBackColor = true;
+            this.Produkt_aendern.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.produkt_id,
+            this.name,
+            this.gewicht,
+            this.preis,
+            this.zeichnungsnummer});
+            this.dataGridView1.Location = new System.Drawing.Point(29, 23);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(690, 637);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // produkt_id
+            // 
+            this.produkt_id.DataPropertyName = "Produkt_ID";
+            this.produkt_id.HeaderText = "ID";
+            this.produkt_id.Name = "produkt_id";
+            this.produkt_id.ReadOnly = true;
+            this.produkt_id.Width = 60;
+            // 
+            // name
+            // 
+            this.name.DataPropertyName = "Name";
+            this.name.HeaderText = "Produktname";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            this.name.Width = 240;
+            // 
+            // gewicht
+            // 
+            this.gewicht.DataPropertyName = "Gewicht";
+            this.gewicht.HeaderText = "Gewicht";
+            this.gewicht.Name = "gewicht";
+            this.gewicht.ReadOnly = true;
+            this.gewicht.Width = 93;
+            // 
+            // preis
+            // 
+            this.preis.DataPropertyName = "Preis";
+            this.preis.HeaderText = "Preis";
+            this.preis.Name = "preis";
+            this.preis.ReadOnly = true;
+            this.preis.Width = 93;
+            // 
+            // zeichnungsnummer
+            // 
+            this.zeichnungsnummer.DataPropertyName = "Zeichnungsnummer";
+            this.zeichnungsnummer.HeaderText = "Zeichnungsnummer";
+            this.zeichnungsnummer.Name = "zeichnungsnummer";
+            this.zeichnungsnummer.ReadOnly = true;
+            this.zeichnungsnummer.Width = 160;
+            // 
+            // Produktliste
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 736);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Produkt_aendern);
+            this.Controls.Add(this.Produkt_hinzufuegen);
+            this.Controls.Add(this.Auswaehlen);
+            this.Controls.Add(this.Schliessen);
+            this.Name = "Produktliste";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Produktliste";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Schliessen;
+        private System.Windows.Forms.Button Auswaehlen;
+        private System.Windows.Forms.Button Produkt_hinzufuegen;
+        private System.Windows.Forms.Button Produkt_aendern;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn produkt_id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gewicht;
+        private System.Windows.Forms.DataGridViewTextBoxColumn preis;
+        private System.Windows.Forms.DataGridViewTextBoxColumn zeichnungsnummer;
+    }
+}
---
+++ Lagerverwaltung/Views/ProduktlisteSuche.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Lagerverwaltung.Views
+{
+    public partial class ProduktlisteSuche : Form
+    {
+        //private Dictionary<long, Model.ProduktModel> _dictProdukt;
+        private Dictionary<long, Model.ProduktModel> _dictProdukt;
+
+        private BindingSource source = new BindingSource();
+
+        public ProduktlisteSuche()
+        {
+            InitializeComponent();
+
+            //dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+
+            /*
+            //TODO: Produkte aus DB oder SW-Liste laden
+            _produktListe = new List<Core.Produkt>
+            {
+                //TEST
+                new Core.Produkt(0, "DeWalt Schrauber", "DT71572-QZ", 0.581f, 11.99f, new DateTime(), new DateTime(), 0, 400, 600, 800),
+                new Core.Produkt(1, "Scheppach Dekupiersägeblatt-Set", "6206P", 0.200f, 19.99f, new DateTime(), new DateTime(), 0, 1000, 2000, 500),
+                new Core.Produkt(2, "Erbauer T144DP Stichsägeblatt", "7153K", 0.055f, 2.19f, new DateTime(), new DateTime(), 0, 300, 100, 100),
+                new Core.Produkt(3, "Ponal SUPER 3 Holzleim", "PN 15S 120", 0.12f, 6.89f, new DateTime(), new DateTime(), 0, 500, 500, 500)
+                
+            };
+            */
+
+            UpdateForm();
+			
+      
+        }
+		public void UpdateForm()
+        {
+
+            _dictProdukt = DB.SqlStatements.HoleProdukt();
+            //dataGridView1.DataSource = Model.Produkt.GetProdukts();
+            dataGridView1.DataSource = _dictProdukt.Values.ToArray();
+
+            dataGridView1.Columns[2].Visible = false;
+            dataGridView1.Columns[3].Visible = false;
+            
+
+            dataGridView1.Columns[7].Visible = false;
+            dataGridView1.Columns[8].Visible = false;
+            dataGridView1.Columns[9].Visible = false;
+            dataGridView1.Columns[10].Visible = false;
+            /*
+			listViewProduktliste.Items.Clear();
+            var test = Model.Produkt.GetProdukts();
+
+			foreach (Model.Produkt produkt in test)
+			{
+				dataGridView1.Items.Add(new ListViewItem(new string[] {
+					produkt.Name,
+					produkt.Gewicht.ToString(),
+					produkt.Preis.ToString(),
+					produkt.Zeichnungsnummer,
+					produkt.ErstelltAm.ToString(),
+					produkt.GeaendertAm.ToString() }));
+			}
+            if(listViewProduktliste.Items.Count>0)
+            {
+                listViewProduktliste.Items[0].Focused = true;
+                listViewProduktliste.Items[0].Selected = true;
+            }*/
+        }
+
+        private void Schliessen_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Produkt_hinzufuegen_Click(object sender, EventArgs e)
+        {
+            using (var produkthinzufuegen = new ProduktHinzufuegen())
+            {
+                produkthinzufuegen.Owner = this;
+                produkthinzufuegen.ShowDialog();
+            }
+        }
+
+		/// <summary>
+		/// Produkt ausgewählt
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Auswaehlen_Click(object sender, EventArgs e)
+		{
+            int selectedrowindex = dataGridView1.SelectedCells[0].RowIndex;
+            DataGridViewRow selectedRow = dataGridView1.Rows[selectedrowindex];
+            string a = Convert.ToString(selectedRow.Cells["name"].Value);
+
+            PaketHinzufuegen.Auswahl = a;
+
+            PaketHinzufuegen.ProduktID = Convert.ToInt64(selectedRow.Cells["produkt_id"].Value);
+
+            Close();
+		}
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int selectedrowindex = dataGridView1.SelectedCells[0].RowIndex;
+            DataGridViewRow selectedRow = dataGridView1.Rows[selectedrowindex];
+
+            ProduktAendern.produkt_id = Convert.ToString(selectedRow.Cells["produkt_id"].Value);
+            ProduktAendern.name = Convert.ToString(selectedRow.Cells["name"].Value);
+            ProduktAendern.zeichnungsnummer = Convert.ToString(selectedRow.Cells["zeichnungsnummer"].Value);
+            ProduktAendern.gewicht = Convert.ToString(selectedRow.Cells["gewicht"].Value);
+            ProduktAendern.preis = Convert.ToString(selectedRow.Cells["preis"].Value);
+            ProduktAendern.hoehe = Convert.ToString(selectedRow.Cells["hoehe"].Value);
+            ProduktAendern.breite = Convert.ToString(selectedRow.Cells["breite"].Value);
+            ProduktAendern.laenge = Convert.ToString(selectedRow.Cells["laenge"].Value);
+            ProduktAendern.artikeltyp_id = Convert.ToString(selectedRow.Cells["artikeltyp_id"].Value);
+
+            using (var produktaendern = new ProduktAendern())
+            {
+                produktaendern.Owner = this;
+                produktaendern.ShowDialog();
+            }
+        }
+    }
+
+    
+}
