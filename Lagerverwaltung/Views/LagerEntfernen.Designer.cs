7841abdc2f1674e6ed62b7025e8fa23b3e21f0bd
CKextreme <CKextreme@users.noreply.github.com>
2019-01-07 12:41:15 +0100

    LagerEntfernen-Form neu

--- Lagerverwaltung/Views/LagerEntfernen.Designer.cs
+++ Lagerverwaltung/Views/LagerEntfernen.Designer.cs
@@ -28,77 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.comboBoxLager = new System.Windows.Forms.ComboBox();
-            this.buttonEntfernen = new System.Windows.Forms.Button();
-            this.buttonAbbrechen = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.LagerLabel = new MetroFramework.Controls.MetroLabel();
+            this.LagerCombobox = new MetroFramework.Controls.MetroComboBox();
+            this.HinzufuegenButton = new MetroFramework.Controls.MetroButton();
+            this.AbbrechenButton = new MetroFramework.Controls.MetroButton();
+            this.metroStyleManager1 = new MetroFramework.Components.MetroStyleManager(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // LagerLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(22, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(164, 26);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Lager entfernen";
+            this.LagerLabel.AutoSize = true;
+            this.LagerLabel.Location = new System.Drawing.Point(23, 82);
+            this.LagerLabel.Name = "LagerLabel";
+            this.LagerLabel.Size = new System.Drawing.Size(104, 19);
+            this.LagerLabel.TabIndex = 0;
+            this.LagerLabel.Text = "Lager entfernen:";
             // 
-            // label2
+            // LagerCombobox
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(45, 76);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 18);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Lager:";
+            this.LagerCombobox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LagerCombobox.FormattingEnabled = true;
+            this.LagerCombobox.ItemHeight = 23;
+            this.LagerCombobox.Location = new System.Drawing.Point(133, 82);
+            this.LagerCombobox.Name = "LagerCombobox";
+            this.LagerCombobox.Size = new System.Drawing.Size(201, 29);
+            this.LagerCombobox.TabIndex = 1;
+            this.LagerCombobox.UseSelectable = true;
             // 
-            // comboBoxLager
+            // HinzufuegenButton
             // 
-            this.comboBoxLager.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBoxLager.FormattingEnabled = true;
-            this.comboBoxLager.Location = new System.Drawing.Point(137, 73);
-            this.comboBoxLager.Name = "comboBoxLager";
-            this.comboBoxLager.Size = new System.Drawing.Size(195, 26);
-            this.comboBoxLager.TabIndex = 2;
+            this.HinzufuegenButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.HinzufuegenButton.Location = new System.Drawing.Point(178, 144);
+            this.HinzufuegenButton.Name = "HinzufuegenButton";
+            this.HinzufuegenButton.Size = new System.Drawing.Size(75, 23);
+            this.HinzufuegenButton.TabIndex = 2;
+            this.HinzufuegenButton.Text = "Hinzufügen";
+            this.HinzufuegenButton.UseSelectable = true;
+            this.HinzufuegenButton.Click += new System.EventHandler(this.HinzufuegenButton_Click);
             // 
-            // buttonEntfernen
+            // AbbrechenButton
             // 
-            this.buttonEntfernen.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonEntfernen.Location = new System.Drawing.Point(119, 137);
-            this.buttonEntfernen.Name = "buttonEntfernen";
-            this.buttonEntfernen.Size = new System.Drawing.Size(95, 35);
-            this.buttonEntfernen.TabIndex = 3;
-            this.buttonEntfernen.Text = "Entfernen";
-            this.buttonEntfernen.UseVisualStyleBackColor = true;
-            this.buttonEntfernen.Click += new System.EventHandler(this.ButtonEntfernen_Click);
+            this.AbbrechenButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AbbrechenButton.Location = new System.Drawing.Point(259, 144);
+            this.AbbrechenButton.Name = "AbbrechenButton";
+            this.AbbrechenButton.Size = new System.Drawing.Size(75, 23);
+            this.AbbrechenButton.TabIndex = 3;
+            this.AbbrechenButton.Text = "Abbrechen";
+            this.AbbrechenButton.UseSelectable = true;
+            this.AbbrechenButton.Click += new System.EventHandler(this.AbbrechenButton_Click);
             // 
-            // buttonAbbrechen
+            // metroStyleManager1
             // 
-            this.buttonAbbrechen.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonAbbrechen.Location = new System.Drawing.Point(237, 137);
-            this.buttonAbbrechen.Name = "buttonAbbrechen";
-            this.buttonAbbrechen.Size = new System.Drawing.Size(95, 35);
-            this.buttonAbbrechen.TabIndex = 4;
-            this.buttonAbbrechen.Text = "Abbrechen";
-            this.buttonAbbrechen.UseVisualStyleBackColor = true;
-            this.buttonAbbrechen.Click += new System.EventHandler(this.ButtonAbbrechen_Click);
+            this.metroStyleManager1.Owner = null;
             // 
             // LagerEntfernen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(357, 190);
-            this.Controls.Add(this.buttonAbbrechen);
-            this.Controls.Add(this.buttonEntfernen);
-            this.Controls.Add(this.comboBoxLager);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AbbrechenButton);
+            this.Controls.Add(this.HinzufuegenButton);
+            this.Controls.Add(this.LagerCombobox);
+            this.Controls.Add(this.LagerLabel);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "LagerEntfernen";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "LagerEntfernen";
+            this.Resizable = false;
+            this.Text = "Lager entfernen";
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,10 +105,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBoxLager;
-        private System.Windows.Forms.Button buttonEntfernen;
-        private System.Windows.Forms.Button buttonAbbrechen;
+        private MetroFramework.Controls.MetroLabel LagerLabel;
+        private MetroFramework.Controls.MetroComboBox LagerCombobox;
+        private MetroFramework.Controls.MetroButton HinzufuegenButton;
+        private MetroFramework.Controls.MetroButton AbbrechenButton;
+        private MetroFramework.Components.MetroStyleManager metroStyleManager1;
     }
 }
--- Lagerverwaltung/Views/LagerEntfernen.cs
+++ Lagerverwaltung/Views/LagerEntfernen.cs
@@ -3,19 +3,19 @@ using System.Windows.Forms;
 
 namespace Lagerverwaltung.Views
 {
-    public partial class LagerEntfernen : Form
+    public partial class LagerEntfernen : MetroFramework.Forms.MetroForm
     {
         public LagerEntfernen()
         {
             InitializeComponent();
         }
 
-        private void ButtonAbbrechen_Click(object sender, EventArgs e)
+        private void AbbrechenButton_Click(object sender, EventArgs e)
         {
             Close();
         }
 
-        private void ButtonEntfernen_Click(object sender, EventArgs e)
+        private void HinzufuegenButton_Click(object sender, EventArgs e)
         {
             throw new NotImplementedException();
         }
