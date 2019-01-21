5ea945904ee680cbe77423aa092fbe870d323d47
5000totalBTW <43860627+5000totalBTW@users.noreply.github.com>
2019-01-21 14:34:05 +0100

    Produktklassen haben jetzt eigene Fehlerbehandlung und Validierung
    Kein ProduktHinzufuegenController.cs benötigt

--- Lagerverwaltung/Views/ProduktHinzufuegen.cs
+++ Lagerverwaltung/Views/ProduktHinzufuegen.cs
@@ -16,28 +16,9 @@ namespace Lagerverwaltung.Views
             artikeltypBox.ValueMember = "artikeltyp_id";
             artikeltypBox.DisplayMember = "name";
 
-            //_artikeltypListe. = DB.SqlStatements.HoleAlleArtikeltypen();
-
-            //Dashboard.ArtikeltypListe;            
-            //TEST
-
-            //TODO:Artikeltypen aus Datenbank laden
-
-
-            //in ComboBox eintragen (eventuell auch ohne Zwischengespeicherte Liste)
-            /*
-            foreach (Model.ArtikeltypModel artikeltyp in _artikeltypListe)
-            {
-                artikeltypBox.Items.Add(artikeltyp);
-            }*/
-
             _artikeltypDict = DB.SqlStatements.HoleArtikeltyp();
 
             artikeltypBox.DataSource = _artikeltypDict.Values.ToArray();
-
-
-            //artikeltypBox.SelectedIndex = 0;
-
 		}
 
         private void ButtonAbbrechen_Click(object sender, EventArgs e)
@@ -49,7 +30,7 @@ namespace Lagerverwaltung.Views
         {
             if (String.IsNullOrWhiteSpace(tb_Name.Text) || tb_Name.Text.Length > 25)
             {
-                MetroFramework.MetroMessageBox.Show(this, "Produktname überschreitet die zulässige Länge von 25 Zeichen!", "Fehler beim Hinzufügen des Produkts", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MetroFramework.MetroMessageBox.Show(this, "Produktname ist leer oder überschreitet die zulässige Länge von 25 Zeichen!", "Fehler beim Hinzufügen des Produkts", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             else
             {
@@ -71,23 +52,6 @@ namespace Lagerverwaltung.Views
                 }
 
             }
-
-            /*
-            if (Controller.ProduktHinzufuegenController.ValidateData(tb_Name.Text, tb_Zeichnungsnummer.Text, tb_Gewicht.Text, tb_Preis.Text, tb_Hoehe.Text, tb_Breite.Text, tb_Laenge.Text))
-            
-            {
-                DB.SqlStatements.ErstelleProdukt(tb_Name.Text, Convert.ToSingle(tb_Gewicht.Text), Convert.ToSingle(tb_Preis.Text), tb_Zeichnungsnummer.Text, Convert.ToInt64(artikeltypBox.SelectedValue), Convert.ToSingle(tb_Hoehe.Text), Convert.ToSingle(tb_Breite.Text), Convert.ToSingle(tb_Laenge.Text));
-
-                ((Produktliste) Besitzer).UpdateForm();
-
-                Close();
-            }
-
-            //Validierung nicht erfolgreich
-            else
-            {
-
-            }*/
         }
     }
 }
